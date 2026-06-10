406e00b0918ef2a329d389f730288f5c89652358
harish.ps <harish.ps@utimaco.com>
2026-06-10 14:58:43 +0530

    Quote Block Added

---
+++ blocks/quote/quote.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+    const [quoteRow, authorRow] = [...block.children];
+  
+    const blockquote = document.createElement('blockquote');
+    blockquote.append(...quoteRow.firstElementChild.childNodes);
+  
+    const cite = document.createElement('cite');
+    if (authorRow) cite.append(...authorRow.firstElementChild.childNodes);
+  
+    block.replaceChildren(blockquote, cite);
+  }
+  
